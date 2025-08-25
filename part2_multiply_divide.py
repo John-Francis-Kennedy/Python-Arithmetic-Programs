5be4d391056b8df78819d85d514b45909fe8d8dc
John-Francis-Kennedy <johnfrancisk@outlook.com>
2025-08-25 01:25:02 -0400

    Update part2_multiply_divide.py

--- part2_multiply_divide.py
+++ part2_multiply_divide.py
@@ -7,11 +7,12 @@ num2 = float(input("Enter the second number: "))
 # Perform operations
 multiplication = num1 * num2
 
+# Display results
+print("Multiplication result:", multiplication)
+
 if num2 != 0:
     division = num1 / num2
     print("Division result:", division)
 else:
     print("Division by zero is not allowed.")
 
-# Display results
-print("Multiplication result:", multiplication)
